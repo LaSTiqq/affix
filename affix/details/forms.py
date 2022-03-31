aee30508ec832dca4c8da5e4e9ae764fa93dbcd5
lauriss <lavrencij@inbox.lv>
2022-03-31 23:38:20 +0300

    Reworked contact us form and few animations

--- affix/details/forms.py
+++ affix/details/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from captcha.fields import CaptchaField, CaptchaTextInput
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'center-block', 'placeholder': 'Vārds'}))
-    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'center-block', 'placeholder': 'E-pasts'}))
-    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'center-block', 'placeholder': 'Temats'}))
-    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'center-block', 'rows': 5, 'placeholder': 'Teksts'}))
-    captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'center-block-add', 'placeholder': 'Atbilde'}))
+    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'Vārds'}))
+    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'E-pasts'}))
+    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'Temats'}))
+    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'mx-auto', 'rows': 5, 'placeholder': 'Teksts'}))
+    captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'd-block mx-auto', 'placeholder': 'Atbilde (cipars vai skaitlis)'}))
