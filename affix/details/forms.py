f1f3b2d507367850216298ecb963c1ebf81b2af6
lauriss <lavrencij@inbox.lv>
2022-04-21 22:06:33 +0300

    Some additional safety attributes to contact us form

--- affix/details/forms.py
+++ affix/details/forms.py
@@ -2,8 +2,22 @@ from django import forms
 from captcha.fields import CaptchaField, CaptchaTextInput
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'Vārds'}))
-    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'E-pasts'}))
-    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'Temats'}))
-    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'mx-auto', 'rows': 5, 'placeholder': 'Teksts'}))
+    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'Vārds',
+																			'onpaste': 'return false;',
+																			'ondrop': 'return false;',
+																			'autocomplete': 'off'}))
+    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'E-pasts',
+																			'onpaste': 'return false;',
+																			'ondrop': 'return false;',
+																			'autocomplete': 'off'}))
+    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 
+																			'placeholder': 'Temats',
+																			'onpaste': 'return false;',
+																			'ondrop': 'return false;',
+																			'autocomplete': 'off'}))
+    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'mx-auto', 'rows': 5,
+																			'placeholder': 'Teksts',
+																			'onpaste': 'return false;',
+																			'ondrop': 'return false;',
+																			'autocomplete': 'off'}))
     captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'd-block mx-auto', 'placeholder': 'Atbilde (cipars vai skaitlis)'}))
