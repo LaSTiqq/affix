8989409dbf02063670365428d0906d5713cdbb70
lauriss <lavrencij@inbox.lv>
2022-06-23 03:11:34 +0300

    Workaround contact us form, noticed smtp issues - fixed

--- affix/details/forms.py
+++ affix/details/forms.py
@@ -2,22 +2,20 @@ from django import forms
 from captcha.fields import CaptchaField, CaptchaTextInput
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'Vārds',
+    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'form-control ms-auto mt-3', 'placeholder': 'Vārds',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'd-block ms-auto', 'placeholder': 'E-pasts',
+    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'form-control ms-auto mt-3', 'placeholder': 'E-pasts',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'd-block ms-auto', 
-																			'placeholder': 'Temats',
+    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'form-control ms-auto mt-3', 'placeholder': 'Temats',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'mx-auto', 'rows': 5,
-																			'placeholder': 'Teksts',
+    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'form-control me-auto mt-3', 'rows': 5, 'placeholder': 'Teksts',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'd-block mx-auto', 'placeholder': 'Atbilde (cipars vai skaitlis)'}))
+    captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'form-control d-block mx-auto', 'placeholder': 'Atbilde (cipars vai skaitlis)'}))
