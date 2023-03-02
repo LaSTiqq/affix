6bb83bf725cbf5b2bdc35b0025832c91f4c23261
LaSTiqq <lavrencij13@gmail.com>
2023-03-03 00:24:00 +0200

    Form is saved even captcha is failed/not submitted

--- details/views.py
+++ details/views.py
@@ -4,6 +4,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.core.exceptions import ValidationError
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from smtplib import SMTPException
 from .forms import ContactForm
 
 def send(request):
@@ -18,23 +19,25 @@ def send(request):
             html_content = render_to_string('email.html', {
                                             'name': body['name'], 'sender': body['sender'], 'content': body['content']})
             text_content = strip_tags(html_content)
-            email = EmailMultiAlternatives(
-                form.cleaned_data['subject'],
-                text_content,
-                'affixsia@gmail.com',
-                ['affixsia@inbox.lv']
-            )
-            email.attach_alternative(html_content, 'text/html')
-            email.send()
-            if email:
+            try:
+                email = EmailMultiAlternatives(
+                    form.cleaned_data['subject'],
+                    text_content,
+                    'affixsia@gmail.com',
+                    ['affixsia@inbox.lv']
+                )
+                email.attach_alternative(html_content, 'text/html')
+                email.send()
                 messages.success(request, 'Vēstule nosūtīta')
                 return redirect('/#contact_us')
-            else:
-                messages.danger(request, 'Kaut kas nav izdevies, lūdzu, mēģiniet vēlreiz')
-                return redirect('/#contact_us')
+            except SMTPException:
+                messages.error(request, 'Kaut kas nav izdevies, lūdzu, mēģiniet vēlreiz')
+                return render(request, 'details/index.html', {"form": form})
+            return redirect('/#contact_us')
         else:
             messages.warning(request, 'Captcha nebija nospiesta, lūdzu, mēģiniet vēlreiz')
-            return redirect('/#contact_us')
+            return render(request, 'details/index.html', {"form": form})
+        return redirect('/#contact_us')
     else:
         form = ContactForm()
     return render(request, 'details/index.html', {"form": form})
