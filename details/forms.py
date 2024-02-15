041ebaf52ba661e9e486523fb004695319950119
LaSTiqq <lavrencij13@gmail.com>
2024-02-16 00:21:19 +0200

    Simplified CSS file

--- details/forms.py
+++ details/forms.py
@@ -6,13 +6,13 @@ from captcha.widgets import ReCaptchaV2Checkbox
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={
-                           'class': 'form-control bg-transparent ms-auto mt-2', 'placeholder': 'Vārds', 'autocomplete': 'off', 'required': True, 'maxlength': '15'}))
+                           'class': 'form-control bg-transparent ms-auto mt-2', 'placeholder': 'Vārds', 'autocomplete': 'off', 'required': True, 'maxlength': '10'}))
     sender = forms.EmailField(label='E-pasts', widget=forms.EmailInput(
         attrs={'class': 'form-control bg-transparent ms-auto mt-2', 'placeholder': 'E-pasts', 'autocomplete': 'off', 'required': True}))
     subject = forms.CharField(label='Temats', widget=forms.TextInput(
-        attrs={'class': 'form-control bg-transparent ms-auto mt-2', 'placeholder': 'Temats', 'autocomplete': 'off', 'required': True, 'maxlength': '30'}))
+        attrs={'class': 'form-control bg-transparent ms-auto mt-2', 'placeholder': 'Temats', 'autocomplete': 'off', 'required': True, 'maxlength': '25'}))
     content = forms.CharField(label='Teksts', widget=forms.Textarea(
-        attrs={'class': 'form-control bg-transparent me-auto ms-md-2 mt-2 pt-3', 'rows': 5, 'placeholder': 'Teksts', 'autocomplete': 'off', 'required': True}))
+        attrs={'class': 'form-control bg-transparent me-auto ms-md-2 mt-2 pt-3', 'rows': 5, 'placeholder': 'Teksts', 'autocomplete': 'off', 'required': True}), min_length=77)
     captcha = ReCaptchaField(
         label='Captcha',
         widget=ReCaptchaV2Checkbox(
