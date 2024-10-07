58e9489d2ee3a7ab411c92481db35925c8425152
LaSTiqq <lavrencij13@gmail.com>
2024-10-07 23:39:21 +0300

    Old bug fix, get rid of jQuery CDN

--- static/js/script.js
+++ static/js/script.js
@@ -4,25 +4,26 @@ const logoNovember = document.getElementById("logo_november");
 const logoDecember = document.getElementById("logo_december");
 
 const scrollFunction = () => {
-    const scrolled = document.body.scrollTop > 50 || document.documentElement.scrollTop > 50;
+  const scrolled =
+    document.body.scrollTop > 50 || document.documentElement.scrollTop > 50;
 
-    const setLogoSize = (logo, width, height) => {
-      if (logo) {
-        logo.style.width = width;
-        logo.style.height = height;
-      }
-    };
-
-    if (scrolled) {
-      setLogoSize(logoDefault, "130px", "70px");
-      setLogoSize(logoNovember, "130px", "70px");
-      setLogoSize(logoDecember, "145px", "85px");
-    } else {
-      setLogoSize(logoDefault, "160px", "80px");
-      setLogoSize(logoNovember, "160px", "80px");
-      setLogoSize(logoDecember, "175px", "95px");
+  const setLogoSize = (logo, width, height) => {
+    if (logo) {
+      logo.style.width = width;
+      logo.style.height = height;
     }
   };
+
+  if (scrolled) {
+    setLogoSize(logoDefault, "130px", "70px");
+    setLogoSize(logoNovember, "130px", "70px");
+    setLogoSize(logoDecember, "145px", "85px");
+  } else {
+    setLogoSize(logoDefault, "160px", "80px");
+    setLogoSize(logoNovember, "160px", "80px");
+    setLogoSize(logoDecember, "175px", "95px");
+  }
+};
 
 window.onscroll = () => {
   scrollFunction();
@@ -34,14 +35,28 @@ document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((el) => {
 });
 
 // Scroll to certificates
-function scrollToAnchor(aid) {
-  let aTag = $("a[name='" + aid + "']");
-  $("html,body").animate({ scrollTop: aTag.offset().top }, "slow");
-}
+const scrollToAnchor = (aid) => {
+  const aTag = document.querySelector("a[name='" + aid + "']");
 
-$("#help").click(function () {
-  $("#Modal6").modal("hide");
-  scrollToAnchor("certificates");
+  if (aTag) {
+    window.scrollTo({
+      top: aTag.offsetTop,
+      behavior: "smooth",
+    });
+  }
+};
+
+document.getElementById("help").addEventListener("click", () => {
+  const modal = document.getElementById("Modal6");
+  const modalInstance = bootstrap.Modal.getInstance(modal);
+
+  if (modalInstance) {
+    modalInstance.hide();
+  }
+
+  setTimeout(() => {
+    scrollToAnchor("certificates");
+  }, 200);
 });
 
 // Hide navbar after click when navbar collapsed
